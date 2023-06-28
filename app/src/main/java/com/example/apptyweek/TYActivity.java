d7ddbb779b18d83d7a2808c04ee2f50a6745d106
elenaoreilly1 <elena.o.reilly@ericsson.com>
2023-06-28 14:15:41 +0100

    ty week

--- app/src/main/java/com/example/apptyweek/TYActivity.java
+++ app/src/main/java/com/example/apptyweek/TYActivity.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class ThirdActivity extends AppCompatActivity {
+public class TYActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_third);
+        setContentView(R.layout.activity_ty);
     }
 
     public void launchsettings(View v) {
